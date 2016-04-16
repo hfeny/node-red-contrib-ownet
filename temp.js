14a678c2a4a35669640fff1e00de225189c2362e
hfeny <v.volodin@slab-project.ru>
2016-04-17 02:56:17 +0300

    1

--- temp.js
+++ temp.js
@@ -1,8 +1,10 @@
-var ownet = require('./lib/ow').ownet;
+var ownet = require("./lib/ow").ownet;
 var conn = new ownet('rpi1.local',4304);
 
-conn.request('/',8,0);
+var msg = conn.request('/',8,0);
 
-conn.request('/3A.C64F07000000/PIO.A',3,1);
-conn.request('/3A.C64F07000000/PIO.A',2);
+//conn.request('/3A.C64F07000000/PIO.A',3,1);
 
+//var msg = conn.request('/3A.C64F07000000/PIO.A',3,0);
+
+console.log('msg', msg);
