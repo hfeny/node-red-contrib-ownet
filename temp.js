57885b155a8bd694ce618482545a39f285c44863
hfeny <v.volodin@slab-project.ru>
2016-04-25 00:22:17 +0300

    Initial commit

--- temp.js
+++ temp.js
@@ -1,7 +1,7 @@
 /* example usage */
 var ownet = require("./lib/ow");
 var owserver = new ownet.owserver('rpi1.local', 4304);
-
+/*
 owserver.ls('/', function(data){
     console.log(data);
 });
@@ -9,7 +9,7 @@ owserver.ls('/', function(data){
 owserver.read('/28.97CA2E020000/temperature10', function(data){
     console.log(data);
 });
-
-owserver.write('/3A.C64F07000000/PIO.A', 1, function(data){
+*/
+owserver.write('/3A.C64F07000000/PIO.A', 0, function(data){
     console.log(data);
 });
