cef2a70336d28f9a2f91a703613dea29179ee847
hfeny <v.volodin@slab-project.ru>
2016-10-24 21:27:53 +0300

    ui changes

--- ownet.js
+++ ownet.js
@@ -22,15 +22,25 @@ module.exports = function(RED) {
             this.port = this.config.port;
             this.path = n.path;
             this.subpath = n.subpath;
-            this.status({fill:"green",shape:"dot",text:this.host + ':' + this.port});
+            this.topic = n.topic;
+            //this.status({fill:"green",shape:"dot",text:this.host + ':' + this.port});
+            this.status({fill:"green",shape:"dot",text:'connected'});
         } else {
-            this.status({fill:"red",shape:"dot",text:"server not set"});
+            //this.status({fill:"red",shape:"dot",text:"server not set"});
+            this.status({fill:"red",shape:"dot",text:'config not ok'});
         }
         var node = this;
         node.on('input', function (msg) {
             var owserver = new ownet.owserver(node.host, node.port);
             owserver.read(node.path + node.subpath, function(res) {
-                msg.topic = node.path + node.subpath;
+                msg.topic = node.topic || (node.path + node.subpath);
+                /*
+                if(isNumber(+res)!==false){
+                    msg.payload = +res;
+                } else {
+                    msg.payload = null;
+                }
+                */
                 msg.payload = res;
                 node.send(msg);
             })
@@ -47,16 +57,19 @@ module.exports = function(RED) {
             this.port = this.config.port;
             this.path = n.path;
             this.subpath = n.subpath;
-            this.status({fill:"green",shape:"dot",text:this.host + ':' + this.port});
+            this.topic = n.topic;
+            //this.status({fill:"green",shape:"dot",text:this.host + ':' + this.port});
+            this.status({fill:"green",shape:"dot",text:'connected'});
         } else {
-            this.status({fill:"red",shape:"dot",text:"server not set"});
+            //this.status({fill:"red",shape:"dot",text:"server not set"});
+            this.status({fill:"red",shape:"dot",text:'config not ok'});
         }
         var node = this;
         node.on('input', function (msg) {
             var owserver = new ownet.owserver(node.host, node.port);
             var value = msg.payload;
             owserver.write(node.path + node.subpath, value, function(res) {
-                msg.topic = node.path + node.subpath;
+                msg.topic = node.topic || (node.path + node.subpath);
                 if(res < 0){
                     msg.payload = false;
                 } else {
@@ -68,6 +81,11 @@ module.exports = function(RED) {
     }
     RED.nodes.registerType('ownet-write',ownetWriteNode);
 // End ownet device write Node
+// Misc function
+    function isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+//
 
     RED.httpAdmin.get('/ownet/ls/:host/:port/*',function(req,res) {
         var server = new ownet.owserver(req.params.host, req.params.port);
@@ -78,7 +96,7 @@ module.exports = function(RED) {
                 list.push(data[0]);
             } else {
                 data = data.sort().slice();
-                var excluded_subpaths = new RegExp("/(?:address|crc8|errata|family|id|locator|scratchpad|r_[a-z]+)$");
+                var excluded_subpaths = new RegExp("/(?:address|crc8|errata|family|id|locator|scratchpad|alias|type|r_[a-z]+)$");
                 for( var i = 0; i < data.length; i++ ){
                     if(data[i].length > 16){
                         if(!data[i].slice(16).match(excluded_subpaths)){
